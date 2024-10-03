dd51fabe0ce5271532567e8226d861ecbdea91d8
T_Mori <morit0301@gmail.com>
2024-10-04 05:30:35 +0900

    ✨ User authentication

---
+++ src/utils.js
@@ -0,0 +1,35 @@
+const { JsonWebTokenError } = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+
+// トークンを複合するための関数
+function getTokenPayload(token) {
+  // トークン化された物の前の情報（user.id）を複合する
+  return jwt.verify(token, process.env.APP_SECRET);
+}
+
+// ユーザーIDを取得するための関数
+function getUserId(req, authToken) {
+  if (req) {
+    // ヘッダーを確認します。認証権限があります？ 確認する。
+    const authHeader = req.headers.authorization;
+    // 権限があるなら
+    if (authHeader) {
+      const token = authHeader.replace("Bearer", "");
+      if (!token) {
+        throw new Error("トークンが見つかりませんでした");
+      }
+      // そのトークンを複合する
+      const { userId } = getTokenPayload(token);
+      return userId;
+    }
+  } else if (authtoken) {
+    const { userId } = getTokenPayload(authToken);
+    return userId;
+  }
+
+  throw new Error("認証権限がありません");
+}
+
+module.exports = {
+  getUserId,
+};
